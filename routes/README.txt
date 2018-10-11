111ef7a32b08f587f95b81d9e19ad38876e258b0
dave1729 <Dave1729@uw.edu>
2018-10-10 23:30:11 -0700

    testing web-rtc iframes

--- routes/README.txt
+++ routes/README.txt
@@ -3,3 +3,11 @@ start project with express command 'npm start'
 Issue with the process listening to port 3000, not stopping.
   Use 'lsof -1 TCP:3000' to identify the {PID}
   then 'kill -i {PID}' to kill it
+
+
+  myjson bins:
+  10teas
+  giikk
+  nud10
+  10cgqc
+  12uhh0
--- routes/test.js
+++ routes/test.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('test');
+  res.render('web-rtc/index.html');
+});
+
+router.get('/easy', function(req, res, next) {
+  res.render('web-rtc/easy.html');
+});
+
+router.get('/create', function(req, res, next) {
+  res.render('web-rtc/noserve.create.html');
+});
+
+router.get('/join', function(req, res, next) {
+  res.render('web-rtc/noserve.join.html');
 });
 
 module.exports = router;
