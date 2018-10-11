7a07bacd875ffedc0eaeedf47ccad212c4861611
dave1729 <Dave1729@uw.edu>
2018-10-10 23:51:51 -0700

    successful web rtc test

--- routes/test.js
+++ routes/test.js
@@ -6,16 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('web-rtc/index.html');
 });
 
-router.get('/easy', function(req, res, next) {
-  res.render('web-rtc/easy.html');
-});
-
 router.get('/create', function(req, res, next) {
-  res.render('web-rtc/noserve.create.html');
+  res.render('web-rtc/create.html');
 });
 
 router.get('/join', function(req, res, next) {
-  res.render('web-rtc/noserve.join.html');
+  res.render('web-rtc/join.html');
 });
 
 module.exports = router;
