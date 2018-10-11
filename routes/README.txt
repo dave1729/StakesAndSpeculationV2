39ce32725cb3dd271bd3c79357e310b15405d11c
dave1729 <Dave1729@uw.edu>
2018-10-10 21:33:31 -0700

    initial commit

---
+++ routes/README.txt
@@ -0,0 +1,5 @@
+start project with express command 'npm start'
+
+Issue with the process listening to port 3000, not stopping.
+  Use 'lsof -1 TCP:3000' to identify the {PID}
+  then 'kill -i {PID}' to kill it
---
+++ routes/users.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/users', function(req, res, next) {
+  res.send('somehting');
+});
+module.exports = router;
