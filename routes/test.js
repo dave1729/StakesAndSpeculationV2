efc3197b5f348b498eac2ec4002682d5eaf1171f
dave1729 <Dave1729@uw.edu>
2018-10-11 23:56:29 -0700

    making only 1 index file

--- routes/test.js
+++ routes/test.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('web-rtc/index.html');
+  res.render('web-rtc/chat.html');
 });
 
 router.get('/create', function(req, res, next) {
